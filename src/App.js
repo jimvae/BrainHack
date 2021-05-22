6a30b9340e0150bae73d1f65973a98069713902a
Jim Vincent <e0425954@u.nus.edu>
2021-05-22 15:06:26 +0800

    Add Sign In and Complete Front-End with navigation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 
 import 'tachyons';
 import Particles from 'react-particles-js';
@@ -24,7 +26,9 @@ class App extends Component {
     this.state = {
       input: '',
       image: '',
-      box: {}
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -64,23 +68,42 @@ class App extends Component {
     ).then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
     .catch(err => console.log(err)
     );
-  };    
+  };
+  
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState( {isSignedIn: false} );
+    } else if ( route === 'home') {
+      this.setState( {isSignedIn: true} ); 
+    }
+    this.setState({route: route});
+  }
 
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className='particles' params={particlesOptions}/>
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home'
+          ?
+            <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
+            
+          : (route === 'register'
+              ? <Register onRouteChange={this.onRouteChange}/>
+              : <Signin onRouteChange={this.onRouteChange}/> 
+          )          
+        }
       </div>
     )
-
   }
 }
 
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const FaceRecognition = ( { imageUrl, box }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img id='inputimage' alt='photo' src={imageUrl} width='500px' height='auto'/>
+                <img id='inputimage' alt='input' src={imageUrl} width='500px' height='auto'/>
                 <div 
                     className='bounding-box' 
                     style={{top: box.topRow, right: box.rightCol, left: box.leftCol, bottom: box.bottomRow}}></div>
